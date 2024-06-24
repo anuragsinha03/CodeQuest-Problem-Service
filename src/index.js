f5b35b0f4b030900895defddd0905d9e5f4f3835
Anurag Sinha <anuraagrk4@gmail.com>
2024-06-24 10:40:15 +0530

    Feat: Added basic express server setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { PORT } = require("./config/server.config");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+
+app.get("/ping", (req, res) => {
+	return res.json({
+		message: "Problem Service is alive",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server up at PORT: ${PORT}`);
+});
