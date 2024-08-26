20faa0c59c8a649c6f04806f6941a200adf267b8
Anurag Sinha <anuraagrk4@gmail.com>
2024-08-27 02:01:37 +0530

    Feat: Enable CORS

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandler");
 const DBConnector = require("./config/db.config");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
